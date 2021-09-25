bf9568c887688d5e7a2ec5554046e68966714c7f
salarcode <salar2k@gmail.com>
2021-09-25 21:02:00 +1000

    Zekr Search fix

--- MyZekr/src/services/Zekr/ZekrSearch.ts
+++ MyZekr/src/services/Zekr/ZekrSearch.ts
@@ -3,12 +3,11 @@ import { ZekrIndex } from './models/Zekr';
 export function searchZekrIndex(zekrIndexList: ZekrIndex[], searchText: string, limit: number): ZekrIndex[] {
 	var result: ZekrIndex[] = [];
 	var remaining = limit;
-
 	for (const zekr of zekrIndexList) {
 		if (zekr.fullName?.includes(searchText) || zekr.metaTitle?.includes(searchText)) {
 			result.push(zekr);
+			remaining--;
 		}
-		remaining--;
 		if (remaining <= 0) break;
 	}
 	return result;
